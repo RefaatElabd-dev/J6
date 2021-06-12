707812961e22e34184a3f982b4bcb4d781967174
= <=>
2021-06-12 06:38:11 +0200

    Add Brand Controller Api

---
+++ Migrations/20210612041027_Day12JuneFixes.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace J6.Migrations
+{
+    public partial class Day12JuneFixes : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
