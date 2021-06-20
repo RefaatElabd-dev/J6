710b88e159cfc14cf00ebb480b3fdd3bc947b890
mahmoudkhaled7422 <77900131+mahmoudkhaled7422@users.noreply.github.com>
2021-06-20 22:42:59 +0200

    Product Views

--- Controllers/ProductsController.cs
+++ Controllers/ProductsController.cs
@@ -7,16 +7,24 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using J6.DAL.Database;
 using J6.DAL.Entities;
+using J6.DAL.ViewModels;
+using System.IO;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 
 namespace J6.Controllers
 {
     public class ProductsController : Controller
     {
         private readonly DbContainer _context;
+        private readonly IWebHostEnvironment _webHostEnvironment;
+        private readonly IHostingEnvironment _hostingEnvironment;
 
-        public ProductsController(DbContainer context)
+        public ProductsController(DbContainer context, IWebHostEnvironment hostEnvironment, IHostingEnvironment hostingEnvironment)
         {
             _context = context;
+            _webHostEnvironment = hostEnvironment;
+            _hostingEnvironment = hostingEnvironment;
         }
 
         // GET: Products
@@ -50,9 +58,9 @@ namespace J6.Controllers
         // GET: Products/Create
         public IActionResult Create()
         {
-            ViewData["BrandId"] = new SelectList(_context.Brands, "BrandId", "BrandId");
+            ViewData["BrandId"] = new SelectList(_context.Brands, "BrandId", "BrandName");
             ViewData["PromotionId"] = new SelectList(_context.Promotions, "PromotionId", "PromotionId");
-            ViewData["SubcategoryId"] = new SelectList(_context.SubCategories, "SubcategoryId", "SubcategoryId");
+            ViewData["SubcategoryId"] = new SelectList(_context.SubCategories, "SubcategoryId", "SubcategoryName");
             //ViewData["Size"] = new SelectList();
 
             return View();
@@ -63,18 +71,63 @@ namespace J6.Controllers
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("Id,Price,SoldQuantities,Quantity,Image,Color,Size,SubcategoryId,ProductName,Model,Rating,Discount,Description,Ship,CreatedAt,UpdatedAt,DeletedAt,material,BrandId,PromotionId,SubcategoryId,Manufacture")] Product product)
+        public async Task<IActionResult> Create(ProductsEditViewModel promodel)
         {
             if (ModelState.IsValid)
             {
+                string uniqueFileName = UploadedFile(promodel);
+                Product product = new Product
+                {
+                    Price= promodel.Price,
+                    SoldQuantities= promodel.SoldQuantities,
+                    Quantity= promodel.Quantity,
+                    Color= promodel.Color,
+                    Size= promodel.Size,
+                    ProductName= promodel.ProductName,
+                    Model= promodel.Model,
+                    Rating= promodel.Rating,
+                    Discount= promodel.Discount,
+                    Ship= promodel.Ship,
+                    material= promodel.material,
+                    SellerId= promodel.SellerId,
+                    BrandId= promodel.BrandId,
+                    PromotionId= promodel.PromotionId,
+                    SubcategoryId= promodel.SubcategoryId,
+                    Manufacture= promodel.Manufacture,
+                    CreatedAt = DateTime.Now,
+                    UpdatedAt = DateTime.Now,
+                    Image = uniqueFileName,
+                    Description = promodel.Description,
+                };
                 _context.Add(product);
                 await _context.SaveChangesAsync();
                 return RedirectToAction(nameof(Index));
             }
-            ViewData["BrandId"] = new SelectList(_context.Brands, "BrandId", "BrandId", product.BrandId);
-            ViewData["PromotionId"] = new SelectList(_context.Promotions, "PromotionId", "PromotionId", product.PromotionId);
-            ViewData["SubcategoryId"] = new SelectList(_context.SubCategories, "SubcategoryId", "SubcategoryId", product.SubcategoryId);
-            return View(product);
+            ViewData["BrandId"] = new SelectList(_context.Brands, "BrandId", "BrandName");
+            ViewData["PromotionId"] = new SelectList(_context.Promotions, "PromotionId", "PromotionId", promodel.PromotionId);
+            ViewData["SubcategoryId"] = new SelectList(_context.SubCategories, "SubcategoryId", "SubcategoryName");
+            return View();
+        }
+
+        private string UploadedFile(ProductsEditViewModel model)
+        {
+            string uniqueFileName = null;
+
+            if (model.Image != null)
+            {
+                string uploadsFolder = Path.Combine(_webHostEnvironment.WebRootPath, "images");
+                uniqueFileName = Guid.NewGuid().ToString() + "_" + model.Image.FileName;
+                string filePath = Path.Combine(uploadsFolder, uniqueFileName);
+                using (var fileStream = new FileStream(filePath, FileMode.Create))
+                {
+                    model.Image.CopyTo(fileStream);
+                }
+            }
+            else
+            {
+                uniqueFileName = "100c4b49-f8ab-4272-988e-1739500fc52e_No-Photo-Available.jpg";
+            }
+            return uniqueFileName;
         }
 
         // GET: Products/Edit/5
@@ -85,15 +138,43 @@ namespace J6.Controllers
                 return NotFound();
             }
 
-            var product = await _context.Products.FindAsync(id);
-            if (product == null)
+            //var product = await _context.Products.FindAsync(id);
+            //if (product == null)
+            //{
+            //    return NotFound();
+            //}
+            
+            Product product = await _context.Products.Where(x => x.Id == id).FirstOrDefaultAsync();
+
+            ProductsEditViewModel viewModel = new ProductsEditViewModel
             {
-                return NotFound();
-            }
-            ViewData["BrandId"] = new SelectList(_context.Brands, "BrandId", "BrandId", product.BrandId);
+
+                Price = product.Price,
+                SoldQuantities = product.SoldQuantities,
+                Quantity = product.Quantity,
+                Color = product.Color,
+                Size = product.Size,
+                ProductName = product.ProductName,
+                Model = product.Model,
+                Rating = product.Rating,
+                Discount = product.Discount,
+                Ship = product.Ship,
+                material = product.material,
+                SellerId = product.SellerId,
+                BrandId = product.BrandId,
+                PromotionId = product.PromotionId,
+                SubcategoryId = product.SubcategoryId,
+                Manufacture = product.Manufacture,
+                CreatedAt = DateTime.Now,
+                UpdatedAt = DateTime.Now,
+                Description = product.Description,
+            };
+            ViewData["BrandId"] = new SelectList(_context.Brands, "BrandId", "BrandName");
             ViewData["PromotionId"] = new SelectList(_context.Promotions, "PromotionId", "PromotionId", product.PromotionId);
-            ViewData["SubcategoryId"] = new SelectList(_context.SubCategories, "SubcategoryId", "SubcategoryId", product.SubcategoryId);
-            return View(product);
+            ViewData["SubcategoryId"] = new SelectList(_context.SubCategories, "SubcategoryId", "SubcategoryName");
+
+            return View(viewModel);
+            //return View(product);
         }
 
         // POST: Products/Edit/5
@@ -101,37 +182,57 @@ namespace J6.Controllers
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("Id,Price,SoldQuantities,SubcategoryId,Quantity,Image,Color,Size,ProductName,Model,Rating,Discount,Description,Ship,CreatedAt,UpdatedAt,DeletedAt,material,BrandId,PromotionId,SubcategoryId,Manufacture")] Product product)
+        public async Task<IActionResult> Edit(int id, ProductsEditViewModel promodel, IFormFile file)
         {
-            if (id != product.Id)
-            {
-                return NotFound();
-            }
+            //if (id != promodel.Id)
+            //{
+            //    return NotFound();
+            //}
 
             if (ModelState.IsValid)
             {
-                try
+                if (id != promodel.Id)
                 {
-                    _context.Update(product);
-                    await _context.SaveChangesAsync();
+                    return NotFound();
                 }
-                catch (DbUpdateConcurrencyException)
+                Product product = await _context.Products.Where(x => x.Id == id).FirstOrDefaultAsync();
+                 product.Price= promodel.Price;
+                 product.SoldQuantities= promodel.SoldQuantities ;
+                 product.Quantity= promodel.Quantity ;
+                 product.Color= promodel.Color ;
+                 product.Size= promodel.Size ;
+                product.ProductName = promodel.ProductName;
+                 product.Model= promodel.Model;
+               product.Rating = promodel.Rating ;
+                product.Discount=promodel.Discount ;
+                product.Ship= promodel.Ship ;
+                product.material= promodel.material ;
+                product.SellerId=promodel.SellerId ;
+               product.BrandId = promodel.BrandId ;
+                 product.PromotionId= promodel.PromotionId;
+                product.SubcategoryId = promodel.SubcategoryId  ;
+                product.Manufacture= promodel.Manufacture ;
+                product.CreatedAt = DateTime.Now;
+                product.UpdatedAt = DateTime.Now;
+                product.Description= promodel.Description ;
+
+                if (promodel.Image != null)
                 {
-                    if (!ProductExists(product.Id))
+                    if (promodel.Image != null)
                     {
-                        return NotFound();
+                        string filepath = Path.Combine(_hostingEnvironment.WebRootPath, "images", promodel.Image.ToString());
+                        System.IO.File.Delete(filepath);
                     }
-                    else
-                    {
-                        throw;
-                    }
+                    product.Image = UploadedFile(promodel);
                 }
+                _context.Update(product);
+                await _context.SaveChangesAsync();
                 return RedirectToAction(nameof(Index));
             }
-            ViewData["BrandId"] = new SelectList(_context.Brands, "BrandId", "BrandId", product.BrandId);
-            ViewData["PromotionId"] = new SelectList(_context.Promotions, "PromotionId", "PromotionId", product.PromotionId);
-            ViewData["SubcategoryId"] = new SelectList(_context.SubCategories, "SubcategoryId", "SubcategoryId", product.SubcategoryId);
-            return View(product);
+            ViewData["BrandId"] = new SelectList(_context.Brands, "BrandId", "BrandName");
+            ViewData["PromotionId"] = new SelectList(_context.Promotions, "PromotionId", "PromotionId", promodel.PromotionId);
+            ViewData["SubcategoryId"] = new SelectList(_context.SubCategories, "SubcategoryId", "SubcategoryName");
+            return View();
         }
 
         // GET: Products/Delete/5
---
+++ DAL/ViewModels/ProductsViewModel.cs
@@ -0,0 +1,61 @@
+﻿using J6.DAL.Entities;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace J6.DAL.ViewModels
+{
+    public class ProductsViewModel
+    {
+
+        [Key]
+        public int Id { get; set; }
+        public double Price { get; set; }
+        public int? SoldQuantities { get; set; }
+        public int? Quantity { get; set; }
+
+        public IFormFile Image { get; set; }
+        public string Color { get; set; }
+        public Size Size { get; set; }
+        public string ProductName { get; set; }
+        public string Model { get; set; }
+        public double? Rating { get; set; }
+        public double? Discount { get; set; }
+        public string Description { get; set; }
+        public ShappedType Ship { get; set; }
+        public DateTime? CreatedAt { get; set; }
+        public DateTime? UpdatedAt { get; set; }
+        public DateTime? DeletedAt { get; set; }
+        public string material { set; get; }
+
+        public int? SellerId { get; set; }
+        [ForeignKey("SellerId")]
+        public virtual AppUser Seller { get; set; }
+
+
+        [ForeignKey("Brands")]
+        public int? BrandId { get; set; }
+        public virtual Brand Brand { get; set; }
+
+        [ForeignKey("Promotion")]
+        public int? PromotionId { get; set; }
+        public virtual Promotion Promotion { get; set; }
+
+        [ForeignKey("Subcategory")]
+        public int? SubcategoryId { get; set; }
+        public virtual SubCategory Subcategory { get; set; }
+
+        public string Manufacture { get; set; }
+        public virtual ICollection<ProdCart> ProdCarts { get; set; }
+        public virtual ICollection<ProdOrder> ProdOrders { get; set; }
+        public virtual ICollection<Review> Reviews { get; set; }
+
+        public virtual ICollection<View> Views { get; set; }
+        public virtual ICollection<MiddleSavedProduct> ProductsBag { get; set; }
+
+    }
+}
