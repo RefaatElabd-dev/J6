28bf6cba9b8c8d8c91acdac889ec89faec08cb08
mahmoudkhaled7422 <77900131+mahmoudkhaled7422@users.noreply.github.com>
2021-06-29 05:43:48 +0200

    Categories data seeds

--- Controllers/OrdersController.cs
+++ Controllers/OrdersController.cs
@@ -24,21 +24,46 @@ namespace J6.Controllers
         [HttpGet]
         public async Task<IActionResult> getAllOrders()
         {
-           //  return await _orderServices.getAllOrders();
+            //  return await _orderServices.getAllOrders();
+            ViewData["OrderStatus"] = new SelectList(_context.Orders, "Status", "Status");
+
             return View(await _orderServices.getAllOrders());
         }
         //Handle Admin Status of orders
-        
+
         //[HttpGet]
         //public async Task<IActionResult> HandleAdminStatus(int OrderId)
         //{
         //    return View(await _orderServices.getAllOrders());
         //}
-        [HttpPost]
-        public async Task<IActionResult> HandleAdminStatus(int OrderId, [FromBody] int statusNumber)
+        //[HttpPost]
+        //public async Task<IActionResult> HandleAdminStatus(int OrderId, [FromBody] int statusNumber)
+        //{
+        //    _orderServices.HandleAdminStatus(OrderId, statusNumber);
+        //    return View("getAllOrders");
+        //}
+        [HttpGet]
+        public void HandleAdminStatus(int OrderId, int statusNumber)
         {
-            _orderServices.HandleAdminStatus(OrderId, statusNumber);
-            return View("getAllOrders");
+            OrderStatus status = OrderStatus.InProgress;
+            switch (statusNumber)
+            {
+                case 0:
+                    status = OrderStatus.InProgress;
+                    break;
+                case 1:
+                    status = OrderStatus.InDelivery;
+                    break;
+                case 2:
+                    status = OrderStatus.Done;
+                    break;
+                default:
+                    break;
+            }
+            Order order = _context.Orders.FirstOrDefault(O => O.Id == OrderId);
+            order.Status = OrderStatus.Done;
+            _context.Orders.Update(order);
+            _context.SaveChanges();
         }
     }
 }
