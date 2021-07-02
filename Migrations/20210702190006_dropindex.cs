505a2d4165720e5a7f8ec33746bfed8d6974370a
RefaatElabd <Refaat.Elabd@Smartware.ae>
2021-07-02 21:07:58 +0200

    fix cart and indexes

---
+++ Migrations/20210702190006_dropindex.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace J6.Migrations
+{
+    public partial class dropindex : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_cart_CustimerId",
+                table: "cart" );
+
+            migrationBuilder.DropIndex(
+               name: "IX_Orders_CustimerId",
+                table: "Orders" );
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
